48fccc9919e752e6e5b322e629c13a6002b7e63b
abdulkerim.karaman <abdulkerim.karaman@albarakatech.com>
2025-03-27 10:33:30 +0300

    Initial Commit

---
+++ SecureApp/SecureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureAppApp.swift
+//  SecureApp
+//
+//  Created by Abdulkerim Karaman on 27.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SecureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
